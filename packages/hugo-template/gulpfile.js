4d7763832f039ee4606846b2f86b31dead5c48b2
Joe Scherben <joe.scherben@informatics-studio.com>
2020-02-03 16:44:50 -0500

    Started to think about how to setup library packages (jquery, front-end plugins)

--- packages/hugo-template/gulpfile.js
+++ packages/hugo-template/gulpfile.js
@@ -1,4 +1,1 @@
-const { init } = require("@ulu/gulp-init");
-
 module.exports = require("@ulu/gulp-init").init();
-// console.log('connected');
---
+++ packages/jquery-prototypes/rollup.config.js
@@ -0,0 +1,23 @@
+import buble from 'rollup-plugin-buble';
+
+let packageJson = require('./package.json');
+let {'jsnext:main': jsnext, main} = packageJson;
+
+export default {
+  moduleName: 'jquery.extras',
+  entry: 'src/extras.js',
+  targets: [{
+    format: 'umd',
+    dest: main
+  }, {
+    format: 'es',
+    dest: jsnext
+  }],
+  globals: {
+    jquery: '$'
+  },
+  external: ['jquery'],
+  plugins: [
+    buble()
+  ]
+};
